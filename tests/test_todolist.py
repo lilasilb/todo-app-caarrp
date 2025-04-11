49c7c4ab06420aa4620019bb02595b22cbbc8293
Boyana Norris <brnorris03@gmail.com>
2025-04-11 12:36:41 -0700

    add CI

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -3,6 +3,12 @@
   but here we use one for simplicity.
 """
 
+import sys
+from pathlib import Path
+
+# Add the parent directory to the Python path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from typing import List
 from todolist import Todo
 
